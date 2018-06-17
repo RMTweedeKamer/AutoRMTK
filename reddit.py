4bbab82cc48ed18307583e8e770105e98a49938b
Stephan Meijer <me@stephanmeijer.com>
2018-06-17 22:31:23 +0200

    Disable spyware

--- reddit.py
+++ reddit.py
@@ -8,4 +8,5 @@ def client():
                       client_id=os.getenv('AUTORMTK_CLIENT_ID'),
                       client_secret=os.getenv('AUTORMTK_CLIENT_SECRET'),
                       username=os.getenv('AUTORMTK_USERNAME'),
-                      password=os.getenv('AUTORMTK_PASSWORD'))
+                      password=os.getenv('AUTORMTK_PASSWORD'),
+                      check_for_updates=False)
